7f5f5c061f9fbbb00bc7386cc6800368183fd765
Your Name <tinus.scheppers@telenet.be>
2018-08-31 14:59:10 +0200

    nav changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-
-import { AppComponent } from './app.component';
-import { NavigationComponent } from './navigation/navigation.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
 import { LayoutModule } from '@angular/cdk/layout';
 import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule } from '@angular/material';
 import { AppRoutingModule } from './/app-routing.module';
+
+import { CardService, UserService } from './core/services/api.service';
+
+import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { UserComponent } from './pages/user/user.component';
 import { LandingComponent } from './pages/landing/landing.component';
+import { ShellComponent } from './shell/shell.component';
 
 @NgModule({
   declarations: [
@@ -19,7 +23,8 @@ import { LandingComponent } from './pages/landing/landing.component';
     HeaderComponent,
     FooterComponent,
     UserComponent,
-    LandingComponent
+    LandingComponent,
+    ShellComponent
   ],
   imports: [
     BrowserModule,
@@ -30,9 +35,13 @@ import { LandingComponent } from './pages/landing/landing.component';
     MatSidenavModule,
     MatIconModule,
     MatListModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    CardService,
+    UserService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/pages/user/user.component.ts
+++ src/app/pages/user/user.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 
+import { UserService, User } from '../../core/services/api.service';
+
+
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.scss']
 })
 export class UserComponent implements OnInit {
+  users: User[];
 
-  constructor() { }
+  constructor(private userService: UserService) { }
 
   ngOnInit() {
+    this.getUsers();
+  }
+
+  getUsers(): void {
+    this.userService.getAll().subscribe(users => this.users = users)
   }
 
 }
